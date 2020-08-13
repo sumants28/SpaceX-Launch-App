13709ec8869b96fd8c222bfe99177ece08654c19
sumants28 <sumants777@gmail.com>
2020-08-14 01:11:17 +0530

    code formatting

--- components/Container/Container.js
+++ components/Container/Container.js
@@ -6,24 +6,23 @@ import classes from "./Container.module.scss";
 
 const Container = (props) => {
 
-        let launch = [];
-        console.log(props)
-        if(props.launches != undefined){
-            launch = props.launches.map(el => {
-                return <Launch key = {el.mission_name} mission = {el}></Launch>
-            })
-        }
-        return (
-            <Fragment>
-                <header className={classes.title}>SpaceX Launch Programs</header>
-                <main className = {classes.container}>
-                    <Filters applyFilter = {props.applyFilter}/>
-                    <div className = {classes.launchContainer}>
-                        {launch}
-                    </div>  
-                </main>
-            </Fragment>  
-        )        
+    let launch = [];
+    if(props.launches != undefined){
+        launch = props.launches.map(el => {
+            return <Launch key = {el.mission_name} mission = {el}></Launch>
+        })
+    }
+    return (
+        <Fragment>
+            <header className={classes.title}>SpaceX Launch Programs</header>
+            <main className = {classes.container}>
+                <Filters applyFilter = {props.applyFilter}/>
+                <div className = {classes.launchContainer}>
+                    {launch}
+                </div>  
+            </main>
+        </Fragment>  
+    )        
 }
 
 export default Container;
